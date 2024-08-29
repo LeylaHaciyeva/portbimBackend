1fc57dd644becb6ba1c154dc7ae7014683777a55
Leyla <l.hajizadeh21@gmail.com>
2024-08-29 18:02:35 +0400

    career

--- src/main/java/az/code/portbim/model/Contact.java
+++ src/main/java/az/code/portbim/model/Contact.java
@@ -1,4 +1,30 @@
 package az.code.portbim.model;
+import java.util.Date;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+@Entity
+@Table(name = "contact")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private   Integer id;
+    private   String name;
+    private   String email;
+    private   String phone;
+    @Column(length = 2000)
+    private   String message;
+    private Date postedDate;
+    @PrePersist
+    protected void onCreate() {
+        if (postedDate == null) {
+            postedDate = new Date(); // Set the current date
+        }
+    }
 }
--- src/main/java/az/code/portbim/model/News.java
+++ src/main/java/az/code/portbim/model/News.java
@@ -1,4 +1,28 @@
 package az.code.portbim.model;
+import java.util.Date;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+@Entity
+@Table(name = "news")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private   Integer id;
+    private byte[] mainImage;
+    private   String title;
+    private   String description;
+    private Date postedDate;
+    @PrePersist
+    protected void onCreate() {
+        if (postedDate == null) {
+            postedDate = new Date(); // Set the current date
+        }
+    }
 }
